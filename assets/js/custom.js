e56f972d4913b2622356b9f1f5591e69fc99892b
Ksenia Baranova <baranova-ksenia@mail.ru>
2021-11-02 13:55:36 +0200

    navbar

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,30 @@
 jQuery(function ($) {
   ("use strict");
 
+  //Menu
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() > 50) {
+      $(".navbar-light").addClass("menu-shrink");
+    } else {
+      $(".navbar-light").removeClass("menu-shrink");
+    }
+  });
+  $(".navbar-nav .nav-item .nav-link").on("click", function (event) {
+    let anchor = $(this);
+    $("html", "body").stop.animate(
+      {
+        scrollTop: $(anchor.attr("href")).offset().top - 50,
+      },
+      1500
+    );
+    event.preventDefault();
+  });
+  // $(document).on("click", ".navbar-collapse.show", function (e) {
+  //   if ($(e.target).is("a") && $(e.target).attr("class") != "dropdown-toggle") {
+  //     $(this).collapse("hide");
+  //   }
+  // });
+
   // Preloader
   jQuery(window).on("load", function () {
     jQuery(".loader").fadeOut(500);
