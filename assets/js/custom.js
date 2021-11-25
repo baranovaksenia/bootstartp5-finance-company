41ecf1a82b729bcbefa521fdd02474f6083df082
Ksenia Baranova <baranova-ksenia@mail.ru>
2021-11-25 12:48:12 +0200

    accordion

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -43,6 +43,34 @@ jQuery(function ($) {
     ],
   });
 
+  // Accordion
+  const accordionItemHeaders = document.querySelectorAll(
+    ".accordion-item-header"
+  );
+
+  accordionItemHeaders.forEach((accordionItemHeader) => {
+    accordionItemHeader.addEventListener("click", (event) => {
+      const currentlyActiveAccordionItemHeader = document.querySelector(
+        ".accordion-item-header.active"
+      );
+      if (
+        currentlyActiveAccordionItemHeader &&
+        currentlyActiveAccordionItemHeader !== accordionItemHeader
+      ) {
+        currentlyActiveAccordionItemHeader.classList.toggle("active");
+        currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+      }
+      accordionItemHeader.classList.toggle("active");
+      const accordionItemBody = accordionItemHeader.nextElementSibling;
+      if (accordionItemHeader.classList.contains("active")) {
+        accordionItemBody.style.maxHeight =
+          accordionItemBody.scrollHeight + "px";
+      } else {
+        accordionItemBody.style.maxHeight = 0;
+      }
+    });
+  });
+
   // Preloader
   jQuery(window).on("load", function () {
     jQuery(".loader").fadeOut(500);
